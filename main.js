222e6a78e9a379bc8621060943f557371217558f
Danny Aziz <daanyaal997@gmail.com>
2020-02-05 18:13:55 +0000

    Eslint fixes + APIKey used in Header

--- main.js
+++ main.js
@@ -6,97 +6,123 @@ class HiveAPI {
         if (!this.apiKey) throw new Error('You must provide an API Key');
     }
 
-    async availableInfluncers({format = 'screenName'} = {}) {
+    async availableInfluncers({ format = 'screenName' } = {}) {
         try {
-            let response = await axios.get('https://hive.one/api/v1/influencers/');
-            return response.data.data.available.map(item => item[(format == 'screenName' ? 1 : 0)]);
+            const response = await axios.get(
+                'https://hive.one/api/v1/influencers/',
+                {
+                    headers: {
+                        Authorization: `Token ${this.apiKey}`,
+                    },
+                },
+            );
+            return response.data.data.available.map((item) => item[(format === 'screenName' ? 1 : 0)]);
         } catch (error) {
-            throw new Error(error)
+            throw new Error(error);
         }
     }
 
-    async topInfluencers({cluster = 'Crypto', after = 0} = {}) {
+    async topInfluencers({ cluster = 'Crypto', after = 0 } = {}) {
         try {
-            let response = await axios.get(
+            const response = await axios.get(
                 'https://hive.one/api/v1/influencers/top/',
                 {
                     params: {
                         cluster,
-                        after
-                    }
-                }
+                        after,
+                    },
+                    headers: {
+                        Authorization: `Token ${this.apiKey}`,
+                    },
+                },
             );
-            return response.data.data.people.edges.map(item => item.node);
+            return response.data.data.people.edges.map((item) => item.node);
         } catch (error) {
-            throw new Error(error)
+            throw new Error(error);
         }
     }
 
-    async influencerDetails({influencerId, format = 'screenName', rankType: rank_type = 'all', includeFollowers: include_followers = 0} = {}) {
+    async influencerDetails({
+        influencerId, format = 'screenName', rankType: rank_type = 'all', includeFollowers: include_followers = 0,
+    } = {}) {
         try {
-            let response = await axios.get(
+            const response = await axios.get(
                 `https://hive.one/api/v1/influencers/${(format === 'screenName' ? 'screen_name' : 'id')}/${influencerId}/`,
                 {
                     params: {
                         rank_type,
-                        include_followers
-                    }
-                }
+                        include_followers,
+                    },
+                    headers: {
+                        Authorization: `Token ${this.apiKey}`,
+                    },
+                },
             );
             return response.data.data;
         } catch (error) {
-            throw new Error(error)
+            throw new Error(error);
         }
     }
 
-    async influencerHistory({influencerId, format = 'screenName', rankType: rank_type = 'all'} = {}) {
+    async influencerHistory({ influencerId, format = 'screenName', rankType: rank_type = 'all' } = {}) {
         try {
-            let response = await axios.get(
+            const response = await axios.get(
                 `https://hive.one/api/v1/influencers/${(format === 'screenName' ? 'screen_name' : 'id')}/${influencerId}/history/`,
                 {
                     params: {
                         rank_type,
-                    }
-                }
+                    },
+                    headers: {
+                        Authorization: `Token ${this.apiKey}`,
+                    },
+                },
             );
             return response.data.data;
         } catch (error) {
-            throw new Error(error)
+            throw new Error(error);
         }
     }
 
-    async influencerPodcasts({influencerId, format = 'screenName', appearanceType: appearance_type = 'all', after = 0} = {}) {
+    async influencerPodcasts({
+        influencerId, format = 'screenName', appearanceType: appearance_type = 'all', after = 0,
+    } = {}) {
         try {
-            let response = await axios.get(
+            const response = await axios.get(
                 `https://hive.one/api/v1/influencers/${(format === 'screenName' ? 'screen_name' : 'id')}/${influencerId}/podcasts/`,
                 {
                     params: {
                         appearance_type,
-                        after
-                    }
-                }
+                        after,
+                    },
+                    headers: {
+                        Authorization: `Token ${this.apiKey}`,
+                    },
+                },
             );
             return response.data.data;
         } catch (error) {
-            throw new Error(error)
+            throw new Error(error);
         }
     }
 
-    async influencerBatch({influencerIDS: twitter_ids = [], rankType: rank_type = 'all', includeFollowers: include_followers = 0} = {}) {
+    async influencerBatch({ influencerIDS: twitter_ids = [], rankType: rank_type = 'all', includeFollowers: include_followers = 0 } = {}) {
         try {
-            let response = await axios.get(
-                `https://hive.one/api/v1/influencers/batch/`,
+            const response = await axios.get(
+                'https://hive.one/api/v1/influencers/batch/',
                 {
                     params: {
                         twitter_ids: JSON.stringify(twitter_ids),
                         rank_type,
                         include_followers,
-                    }
-                }
+                    },
+                    headers: {
+                        Authorization: `Token ${this.apiKey}`,
+                    },
+                },
             );
             return response.data.data.success;
         } catch (error) {
-            throw new Error(error)
+            throw new Error(error);
         }
     }
 }
@@ -105,4 +131,4 @@ function createAPI(key) {
     return new HiveAPI(key);
 }
 
-export default createAPI
+export default createAPI;
