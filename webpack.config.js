6f3bb2a48ad982d7949e29da36986224d59242c1
Danny Aziz <daanyaal997@gmail.com>
2020-02-06 12:11:48 +0000

    Added jest

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
 const path = require('path');
 
+const { NODE_ENV = 'development' } = process.env;
+
 module.exports = {
-    mode: 'development',
+    mode: NODE_ENV,
     target: 'node',
     entry: './main.js',
     module: {
         rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-        ]
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+        ],
     },
     output: {
         filename: 'hive.js',
         path: path.resolve(__dirname, 'dist'),
         library: '',
         libraryTarget: 'umd',
-    }
-}
+    },
+};
